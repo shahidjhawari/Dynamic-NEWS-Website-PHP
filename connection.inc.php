5c33199aaf69e68455e59f9ccfe34a5e5c48dc7e
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-03-26 23:13:13 +0500

    set image folders

--- connection.inc.php
+++ connection.inc.php
@@ -3,4 +3,10 @@ session_start();
 $con=mysqli_connect("localhost","root","","news_website");
 define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/news_website/');
 define('SITE_PATH','http://127.0.0.1/news_website/');
+
+define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'media/news_images/');
+define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'media/news_images/');
+
+define('PRODUCT_MULTIPLE_IMAGE_SERVER_PATH',SERVER_PATH.'media/news_multi_images/');
+define('PRODUCT_MULTIPLE_IMAGE_SITE_PATH',SITE_PATH.'media/news_multi_images/');
 ?>
